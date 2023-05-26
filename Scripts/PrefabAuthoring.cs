e8febc9c989a9752b2739fab6756af498e1b12e8
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2023-05-26 13:21:10 +0200

    fixes: `PrefabAuthoring.Baker`

--- Scripts/PrefabAuthoring.cs
+++ Scripts/PrefabAuthoring.cs
@@ -5,11 +5,12 @@ using Unity.Entities;
 [DisallowMultipleComponent]
 public class PrefabAuthoring : MonoBehaviour
 {
-	public class Baker : Baker<PrefabSystemPoolAuthoring>
+	public class Baker : Baker<PrefabAuthoring>
 	{
-		public override void Bake ( PrefabSystemPoolAuthoring authoring )
+		public override void Bake ( PrefabAuthoring authoring )
 		{
 			Entity entity = this.GetEntity( authoring , TransformUsageFlags.None );
+
 			AddComponent<PrefabSystem.RequestPrefabRegistration>( entity );
 			AddComponent<Prefab>( entity );
 		}
