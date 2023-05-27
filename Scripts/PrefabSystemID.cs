a7de46ab5d30a4ffbc5dd6d40f4126f6eb9be975
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2023-05-27 23:41:49 +0200

    added: short <summary> blocks

--- Scripts/PrefabSystemID.cs
+++ Scripts/PrefabSystemID.cs
@@ -3,6 +3,9 @@ using Unity.Collections;
 
 namespace ECSPrefabLookup
 {
+	/// <summary>
+	/// Inner prefab ID data for <seealso cref="PrefabSystem"/>
+	/// </summary>
 	public struct PrefabSystemID : ICleanupComponentData
 	{
 		public FixedString64Bytes Value;
--- Scripts/RequestPrefabPoolRegistration.cs
+++ Scripts/RequestPrefabPoolRegistration.cs
@@ -3,6 +3,10 @@ using Unity.Collections;
 
 namespace ECSPrefabLookup
 {
+	/// <summary>
+	/// Request to register multiple prefabs.
+	/// Consumed by <seealso cref="PrefabSystem"/>.
+	/// </summary>
 	public struct RequestPrefabPoolRegistration : IBufferElementData
 	{
 		public FixedString64Bytes ID;
