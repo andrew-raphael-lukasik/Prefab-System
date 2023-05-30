c5bb819210f14efef424d1b178363de7c59657f0
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2023-05-30 20:27:04 +0200

    few minor changes to `SingletonCreationSystem`

--- Scripts/SingletonCreationSystem.cs
+++ Scripts/SingletonCreationSystem.cs
@@ -8,27 +8,25 @@ public partial struct PrefabSystem
 	/// </summary>
 	[WorldSystemFilter( WorldSystemFilterFlags.LocalSimulation | WorldSystemFilterFlags.ServerSimulation | WorldSystemFilterFlags.ClientSimulation )]
 	[UpdateInGroup( typeof(InitializationSystemGroup) , OrderFirst=true )]
-	[RequireMatchingQueriesForUpdate]
 	public partial struct SingletonCreationSystem : ISystem
 	{
 
-		//[Unity.Burst.BurstCompile]
+		[Unity.Burst.BurstCompile]
 		public void OnCreate ( ref SystemState state )
 		{
-			Entity singleton = state.EntityManager.CreateEntity( ComponentType.ReadWrite<Prefabs>() );
+			Entity singleton = state.EntityManager.CreateEntity();
+			state.EntityManager.AddComponent<Prefabs>( singleton );
 			SystemAPI.SetSingleton( new Prefabs{
 				Registry = new NativeHashMap<FixedString64Bytes,Entity>( 128 , Allocator.Persistent )
 			} );
+			state.Enabled = false;
 		}
 
 		[Unity.Burst.BurstCompile]
-		public void OnDestroy ( ref SystemState state )
-		{
-			if( SystemAPI.TryGetSingletonEntity<Prefabs>(out Entity singleton) )
-				state.EntityManager.DestroyEntity( singleton );
-		}
+		public void OnDestroy ( ref SystemState state ) {}
 
 		[Unity.Burst.BurstCompile]
 		public void OnUpdate ( ref SystemState state ) {}
+
 	}
 }
